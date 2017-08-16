f70550b3fd78ef7b5a881f96a809e4e9f25b3fd8
Sergey Yablokov <yablokov@trueconf.ua>
2017-08-16 15:30:07 +0300

    generalize perf test code

--- math/tests/test_perf.cpp
+++ math/tests/test_perf.cpp
@@ -58,14 +58,14 @@ math::aligned_vector<math::mat4> create_mat4s(size_t N) {
     return mats;
 }
 
-struct test_timer {
+struct scoped_timer {
     const char *name;
     math::e_arch arch;
     std::chrono::system_clock::time_point t1;
 
-    test_timer(const char *_name, math::e_arch _arch) : name(_name), arch(_arch), t1(std::chrono::system_clock::now()) {}
+    scoped_timer(const char *_name, math::e_arch _arch) : name(_name), arch(_arch), t1(std::chrono::system_clock::now()) {}
 
-    ~test_timer() {
+    ~scoped_timer() {
         auto t2 = std::chrono::system_clock::now();
         std::cout << " " << name << ":\t" << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms";
         if (!math::supported(arch)) std::cout << " (not supported)";
@@ -80,7 +80,7 @@ void test_vec2_add(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec2s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -96,7 +96,7 @@ void test_vec2_mul(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec2s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -112,7 +112,7 @@ void test_vec2_dot(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec2s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     volatile float res = 0;
     for (size_t j = 0; j < M; j++) {
@@ -129,7 +129,7 @@ void test_vec2_len(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec2s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     volatile float res = 0;
     for (size_t j = 0; j < M; j++) {
@@ -146,7 +146,7 @@ void test_vec3_add(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec3s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -162,7 +162,7 @@ void test_vec3_mul(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec3s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -178,7 +178,7 @@ void test_vec3_dot(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec3s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     volatile float res = 0;
     for (size_t j = 0; j < M; j++) {
@@ -195,7 +195,7 @@ void test_vec3_cross(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec3s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -211,7 +211,7 @@ void test_vec3_len(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec3s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     volatile float res = 0;
     for (size_t j = 0; j < M; j++) {
@@ -228,7 +228,7 @@ void test_vec3_normalize(const char *name, math::e_arch arch, size_t N, size_t M
 
     auto vecs = create_vec3s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i++) {
@@ -244,7 +244,7 @@ void test_vec4_add(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec4s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -260,7 +260,7 @@ void test_vec4_mul(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec4s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -276,7 +276,7 @@ void test_vec4_dot(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec3s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     volatile float res = 0;
     for (size_t j = 0; j < M; j++) {
@@ -293,7 +293,7 @@ void test_vec4_len(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto vecs = create_vec4s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     volatile float res = 0;
     for (size_t j = 0; j < M; j++) {
@@ -310,7 +310,7 @@ void test_vec4_normalize(const char *name, math::e_arch arch, size_t N, size_t M
 
     auto vecs = create_vec4s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i++) {
@@ -326,7 +326,7 @@ void test_mat2_add(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto mats = create_mat2s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -342,7 +342,7 @@ void test_mat2_mul(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto mats = create_mat2s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -358,7 +358,7 @@ void test_mat3_add(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto mats = create_mat3s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -374,7 +374,7 @@ void test_mat3_mul(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto mats = create_mat3s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -390,7 +390,7 @@ void test_mat3_comp_mul(const char *name, math::e_arch arch, size_t N, size_t M)
 
     auto mats = create_mat3s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -406,7 +406,7 @@ void test_mat4_add(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto mats = create_mat4s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -422,7 +422,7 @@ void test_mat4_mul(const char *name, math::e_arch arch, size_t N, size_t M) {
 
     auto mats = create_mat4s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -438,7 +438,7 @@ void test_mat4_comp_mul(const char *name, math::e_arch arch, size_t N, size_t M)
 
     auto mats = create_mat4s(N);
 
-    test_timer timer = { name, arch };
+    scoped_timer timer = { name, arch };
 
     for (size_t j = 0; j < M; j++) {
         for (size_t i = 0; i < N - 1; i += 2) {
@@ -446,7 +446,6 @@ void test_mat4_comp_mul(const char *name, math::e_arch arch, size_t N, size_t M)
         }
     }
 }
-
 
 void test_perf() {
     using namespace std; using namespace math;
@@ -469,163 +468,43 @@ void test_perf() {
 
     const size_t N = 1000, M = 100000;
 
-    /*cout << "                " << endl;
-    cout << "     [vec2]     " << endl;
-    cout << "==== vec2 add ====" << endl;
-    for (auto a : archs) {
-        test_vec2_add(a.first, a.second, N, M);
+    std::pair<const char *, void(*)(const char *, math::e_arch, size_t, size_t)> tests[] =
+    { 
+        //{ "vec2 add", test_vec2_add },
+        //{ "vec2 mul", test_vec2_mul },
+        //{ "vec2 dot", test_vec2_dot },
+        //{ "vec2 len", test_vec2_len },
+
+        { "vec3 add", test_vec3_add },
+        { "vec3 mul", test_vec3_mul },
+        { "vec3 dot", test_vec3_dot },
+        { "vec3 cross", test_vec3_cross },
+        { "vec3 len", test_vec3_len },
+        { "vec3 norm", test_vec3_normalize},
+
+        { "vec4 add", test_vec4_add },
+        { "vec4 mul", test_vec4_mul },
+        { "vec4 dot", test_vec4_dot },
+        { "vec4 len", test_vec4_len },
+        { "vec4 norm", test_vec4_normalize},
+
+        //{ "mat2 add", test_mat2_add },
+        //{ "mat2 mul", test_mat2_mul },
+
+        { "mat3 add", test_mat3_add },
+        { "mat3 mul", test_mat3_mul },
+        { "mat3 comp mul", test_mat3_comp_mul },
+
+        { "mat4 add", test_mat4_add },
+        { "mat4 mul", test_mat4_mul },
+        { "mat4 comp mul", test_mat4_comp_mul },
+    };
+
+    for (const auto &t : tests) {
+        cout << "==== " << t.first << " ====" << endl;
+        for (auto a : archs) {
+            t.second(a.first, a.second, N, M);
+        }
+        cout << "==================" << endl;
     }
-    cout << "==================" << endl;
-
-    cout << "==== vec2 mul ====" << endl;
-    for (auto a : archs) {
-        test_vec2_mul(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "==== vec2 dot ====" << endl;
-    for (auto a : archs) {
-        test_vec2_dot(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "==== vec2 len ====" << endl;
-    for (auto a : archs) {
-        test_vec2_len(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;*/
-
-    //////////////////////////////////////////////
-
-    cout << "                " << endl;
-    cout << "     [vec3]     " << endl;
-    cout << "==== vec3 add ====" << endl;
-    for (auto a : archs) {
-        test_vec3_add(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "==== vec3 mul ====" << endl;
-    for (auto a : archs) {
-        test_vec3_mul(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "==== vec3 dot ====" << endl;
-    for (auto a : archs) {
-        test_vec3_dot(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "=== vec3 cross ===" << endl;
-    for (auto a : archs) {
-        test_vec3_cross(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "==== vec3 len ===" << endl;
-    for (auto a : archs) {
-        test_vec3_len(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "==== vec3 norm ===" << endl;
-    for (auto a : archs) {
-        test_vec3_normalize(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    //////////////////////////////////////////////
-
-    cout << "                " << endl;
-    cout << "     [vec4]     " << endl;
-    cout << "==== vec4 add ====" << endl;
-    for (auto a : archs) {
-        test_vec3_add(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "==== vec4 mul ====" << endl;
-    for (auto a : archs) {
-        test_vec3_mul(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "==== vec4 dot ====" << endl;
-    for (auto a : archs) {
-        test_vec3_dot(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "==== vec4 len ===" << endl;
-    for (auto a : archs) {
-        test_vec4_len(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "==== vec4 norm ===" << endl;
-    for (auto a : archs) {
-        test_vec4_normalize(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    //////////////////////////////////////////////
-
-    /*cout << "                " << endl;
-    cout << "     [mat2]     " << endl;
-    cout << "=== mat2 add  ===" << endl;
-    for (auto a : archs) {
-        test_mat2_add(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "=== mat2 mul  ===" << endl;
-    for (auto a : archs) {
-        test_mat2_mul(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;*/
-
-    //////////////////////////////////////////////
-
-    cout << "                " << endl;
-    cout << "     [mat3]     " << endl;
-    cout << "=== mat3 add  ===" << endl;
-    for (auto a : archs) {
-        test_mat3_add(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "=== mat3 mul  ===" << endl;
-    for (auto a : archs) {
-        test_mat3_mul(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "= mat3 comp mul  =" << endl;
-    for (auto a : archs) {
-        test_mat3_comp_mul(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    //////////////////////////////////////////////
-
-    cout << "                " << endl;
-    cout << "     [mat4]     " << endl;
-    cout << "=== mat4 add  ===" << endl;
-    for (auto a : archs) {
-        test_mat4_add(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "=== mat4 mul  ===" << endl;
-    for (auto a : archs) {
-        test_mat4_mul(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
-
-    cout << "== mat4 comp mul =" << endl;
-    for (auto a : archs) {
-        test_mat4_comp_mul(a.first, a.second, N, M);
-    }
-    cout << "==================" << endl;
 }
