59928b619d063b4404560c249edd70b4b0c47ac1
Olaf Reusch <apollo0510@hotmail.com>
2017-08-25 15:54:24 +0200

    improve tests cont.

--- math/tests/test_geometric.cpp
+++ math/tests/test_geometric.cpp
@@ -2,92 +2,150 @@
 
 #include "../math.hpp"
 
-void test_geometric(math::e_arch arch) {
+#include <iostream>
+
+#include <random>
+#include "glm/ext.hpp"
+
+void test_geometric(math::e_arch arch, unsigned seed) {
     using namespace math;
 
     init(arch);
 
-    {   // vec2
-        vec2 v0 = { 1, 2 }, v1 = { -1, -2 };
+	{
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_real_distribution<float> dist(-100, 100);
 
-        float l0 = length(v0);
-        float ll0 = length2(v0);
+		std::string s;
+		for (int i = 0; i < 256; i++) {
+			glm::mat4 m1 { dist(gen), dist(gen), dist(gen), dist(gen),
+						   dist(gen), dist(gen), dist(gen), dist(gen),
+						   dist(gen), dist(gen), dist(gen), dist(gen),
+						   dist(gen), dist(gen), dist(gen), dist(gen) };
+			glm::mat4 m2 = glm::inverse(m1);
 
-        float d01 = distance(v0, v1);
-        float dd01 = distance2(v0, v1);
+			for (int j = 0; j < 4; j++) {
+				for (int k = 0; k < 4; k++) {
+					s += std::to_string(m1[j][k]);
+					s += "f, ";
+				}
+			}
 
-        float dot01 = dot(v0, v1);
+			for (int j = 0; j < 4; j++) {
+				for (int k = 0; k < 4; k++) {
+					s += std::to_string(m2[j][k]);
+					s += "f, ";
+				}
+			}
+			s += "\n";
+		}
 
-        assert(l0 == Approx(2.23606));
-        assert(ll0 == Approx(5));
-        assert(d01 == Approx(4.47213));
-        assert(dd01 == Approx(20));
-        assert(dot01 == Approx(-5));
-    }
+		volatile int ii = 0;
+	}
+
+	std::cout << "\t" << math::arch_name[arch] << " ...";
+
+	{	// vec2
+		extern std::vector<float> vec2_geo_test_data;
+
+		for (size_t i = 0; i < vec2_geo_test_data.size(); i += 9) {
+			vec2 v0 = { vec2_geo_test_data[0], vec2_geo_test_data[1] }, v1 = { vec2_geo_test_data[2], vec2_geo_test_data[3] };
+
+			float l0 = length(v0);
+			float ll0 = length2(v0);
+
+			float d01 = distance(v0, v1);
+			float dd01 = distance2(v0, v1);
+
+			float dot01 = dot(v0, v1);
+
+			assert(l0 == Approx(vec2_geo_test_data[4]));
+			assert(ll0 == Approx(vec2_geo_test_data[5]));
+			assert(d01 == Approx(vec2_geo_test_data[6]));
+			assert(dd01 == Approx(vec2_geo_test_data[7]));
+			assert(dot01 == Approx(vec2_geo_test_data[8]));
+		}
+	}
 
     {   // vec3
-        vec3 v0 = { 1, 2, 3 }, v1 = { -1, -2, -3 };
+		extern std::vector<float> vec3_geo_test_data;
 
-        float l0 = length(v0);
-        float ll0 = length2(v0);
+		for (size_t i = 0; i < vec3_geo_test_data.size(); i += 17) {
+			vec3 v0 = { vec3_geo_test_data[0], vec3_geo_test_data[1], vec3_geo_test_data[2] },
+				 v1 = { vec3_geo_test_data[3], vec3_geo_test_data[4], vec3_geo_test_data[5] };
 
-        float d01 = distance(v0, v1);
-        float dd01 = distance2(v0, v1);
+			float l0 = length(v0);
+			float ll0 = length2(v0);
 
-        float dot01 = dot(v0, v1);
+			float d01 = distance(v0, v1);
+			float dd01 = distance2(v0, v1);
 
-        assert(l0 == Approx(3.74165));
-        assert(ll0 == Approx(14));
-        assert(d01 == Approx(7.48331));
-        assert(dd01 == Approx(56));
-        assert(dot01 == Approx(-14));
+			float dot01 = dot(v0, v1);
 
-        vec3 v2 = { 2, 3, 4 }, v3 = { 5, 6, 7 };
+			assert(l0 == Approx(vec3_geo_test_data[6]));
+			assert(ll0 == Approx(vec3_geo_test_data[7]));
+			assert(d01 == Approx(vec3_geo_test_data[8]));
+			assert(dd01 == Approx(vec3_geo_test_data[9]));
+			assert(dot01 == Approx(vec3_geo_test_data[10]));
 
-        vec3 cross23 = cross(v2, v3);
+			vec3 cross01 = cross(v0, v1);
 
-        assert(cross23[0] == Approx(-3));
-        assert(cross23[1] == Approx(6));
-        assert(cross23[2] == Approx(-3));
+			assert(cross01[0] == Approx(vec3_geo_test_data[11]));
+			assert(cross01[1] == Approx(vec3_geo_test_data[12]));
+			assert(cross01[2] == Approx(vec3_geo_test_data[13]));
 
-        vec3 v4 = normalize(v0), v5 = normalize_fast(v1);
+			vec3 v2 = normalize(v0), v3 = normalize_fast(v0);
 
-        assert(v4 == Approx3(0.26726124, 0.53452248, 0.80178372));
-        assert(v5 == Approx3(-0.26726124, -0.53452248, -0.80178372).epsilon(0.001));
+			assert(v2 == Approx3(vec3_geo_test_data[14], vec3_geo_test_data[15], vec3_geo_test_data[16]));
+			assert(v3 == Approx3(vec3_geo_test_data[14], vec3_geo_test_data[15], vec3_geo_test_data[16]).epsilon(0.001));
+		}
     }
 
     {   // vec4
-        vec4 v0 = { 1, 2, 3, 4 }, v1 = { -1, -2, -3, -4 };
+		extern std::vector<float> vec4_geo_test_data;
 
-        float l0 = length(v0);
-        float ll0 = length2(v0);
+		for (size_t i = 0; i < vec4_geo_test_data.size(); i += 17) {
+			vec4 v0 = { vec4_geo_test_data[0], vec4_geo_test_data[1], vec4_geo_test_data[2], vec4_geo_test_data[3] },
+				 v1 = { vec4_geo_test_data[4], vec4_geo_test_data[5], vec4_geo_test_data[6], vec4_geo_test_data[7] };
 
-        float d01 = distance(v0, v1);
-        float dd01 = distance2(v0, v1);
+			float l0 = length(v0);
+			float ll0 = length2(v0);
 
-        float dot01 = dot(v0, v1);
+			float d01 = distance(v0, v1);
+			float dd01 = distance2(v0, v1);
 
-        assert(l0 == Approx(5.47722));
-        assert(ll0 == Approx(30));
-        assert(d01 == Approx(10.95445));
-        assert(dd01 == Approx(120));
-        assert(dot01 == Approx(-30));
+			float dot01 = dot(v0, v1);
 
-        vec4 v2 = normalize(v0), v3 = normalize_fast(v1);
+			assert(l0 == Approx(vec4_geo_test_data[8]));
+			assert(ll0 == Approx(vec4_geo_test_data[9]));
+			assert(d01 == Approx(vec4_geo_test_data[10]));
+			assert(dd01 == Approx(vec4_geo_test_data[11]));
+			assert(dot01 == Approx(vec4_geo_test_data[12]));
 
-        assert(v2 == Approx4(0.18257418, 0.36514837, 0.54772255, 0.73029674));
-        assert(v3 == Approx4(-0.18257418, -0.36514837, -0.54772255, -0.73029674));
-    }
+			vec4 v2 = normalize(v0), v3 = normalize_fast(v0);
+
+			assert(v2 == Approx4(vec4_geo_test_data[13], vec4_geo_test_data[14], vec4_geo_test_data[15], vec4_geo_test_data[16]));
+			assert(v3 == Approx4(vec4_geo_test_data[13], vec4_geo_test_data[14], vec4_geo_test_data[15], vec4_geo_test_data[16]));
+		}
+	}
+
+	std::cout << "OK" << std::endl;
 }
 
 void test_geometric() {
-    test_geometric(math::Scalar);
+	std::random_device rd;
+	auto seed = rd();
+
+	std::cout << "test_geometric (seed " << seed << ")" << std::endl;
+
+    test_geometric(math::Scalar, seed);
 #ifndef __arm__
-    test_geometric(math::SSE2);
-    test_geometric(math::SSE3);
-    test_geometric(math::SSE4_1);
-    test_geometric(math::AVX);
+    test_geometric(math::SSE2, seed);
+    test_geometric(math::SSE3, seed);
+    test_geometric(math::SSE4_1, seed);
+    test_geometric(math::AVX, seed);
 #else
-    test_geometric(math::NEON);
+    test_geometric(math::NEON, seed);
 #endif
 }
--- math/tests/test_transform.cpp
+++ math/tests/test_transform.cpp
@@ -2,77 +2,112 @@
 
 #include "../math.hpp"
 
-void test_transform(math::e_arch arch) {
+#include <iostream>
+#include <random>
+
+void test_transform(math::e_arch arch, unsigned seed) {
     using namespace math;
 
     init(arch);
 
-    {   // test translate
+	const int NUM_REPEATS = 1000;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<float> dist(-100, 100);
+
+	std::cout << "\t" << math::arch_name[arch] << " ...";
+
+    // test translate
+	for (int i = 0; i < NUM_REPEATS; i++) {
+		float r1 = dist(gen), r2 = dist(gen), r3 = dist(gen);
+
         mat4 m1;
-        mat4 m2 = translate(m1, { 1, 2, 3 });
+        mat4 m2 = translate(m1, { r1, r2, r3 });
 
         assert(m2[0] == Approx4(1, 0, 0, 0));
         assert(m2[1] == Approx4(0, 1, 0, 0));
         assert(m2[2] == Approx4(0, 0, 1, 0));
-        assert(m2[3] == Approx4(1, 2, 3, 1));
+        assert(m2[3] == Approx4(r1, r2, r3, 1));
     }
 
     {   // test rotate
-        mat4 m1;
-        mat4 m2 = rotate(m1, radians(45), { 0, 1, 0 });
+		extern std::vector<float> mat_rotate_test_data;
 
-        assert(m2[0] == Approx4(0.707106, 0, -0.707106, 0));
-        assert(m2[1] == Approx4(0, 1, 0, 0));
-        assert(m2[2] == Approx4(0.707106, 0, 0.707106, 0));
-        assert(m2[3] == Approx4(0, 0, 0, 1));
+		for (size_t i = 0; i < mat_rotate_test_data.size(); i += 20) {
+			mat4 m1;
+			mat4 m2 = rotate(m1, radians(mat_rotate_test_data[0]), make_vec3(&mat_rotate_test_data[1]));
+
+			assert(m2[0] == Approx4(mat_rotate_test_data[4], mat_rotate_test_data[5], mat_rotate_test_data[6], mat_rotate_test_data[7]));
+			assert(m2[1] == Approx4(mat_rotate_test_data[8], mat_rotate_test_data[9], mat_rotate_test_data[10], mat_rotate_test_data[11]));
+			assert(m2[2] == Approx4(mat_rotate_test_data[12], mat_rotate_test_data[13], mat_rotate_test_data[14], mat_rotate_test_data[15]));
+			assert(m2[3] == Approx4(mat_rotate_test_data[16], mat_rotate_test_data[17], mat_rotate_test_data[18], mat_rotate_test_data[19]));
+		}
     }
 
-    {   // test scale
-        mat4 m1;
-        mat4 m2 = scale(m1, { 1, 2, 3 });
+    // test scale
+	for (int i = 0; i < NUM_REPEATS; i++) {
+		float r1 = dist(gen), r2 = dist(gen), r3 = dist(gen);
 
-        assert(m2[0] == Approx4(1, 0, 0, 0));
-        assert(m2[1] == Approx4(0, 2, 0, 0));
-        assert(m2[2] == Approx4(0, 0, 3, 0));
+        mat4 m1;
+        mat4 m2 = scale(m1, { r1, r2, r3 });
+
+        assert(m2[0] == Approx4(r1, 0, 0, 0));
+        assert(m2[1] == Approx4(0, r2, 0, 0));
+        assert(m2[2] == Approx4(0, 0, r3, 0));
         assert(m2[3] == Approx4(0, 0, 0, 1));
     }
 
     {   // test inverse
-        mat2 m2 = { 1, 2, 3, 4 };
-        mat3 m3 = { 1, 2, 3,
-                    4, 1, 2,
-                    3, 4, 1 };
-        mat4 m4 = { 1, 2, 3, 4,
-                    1, 2, 1, 2,
-                    1, 1, 1, 1,
-                    3, 2, 1, 1 };
+		extern std::vector<float> mat2_inverse_test_data;
 
-        mat2 im2 = inverse(m2);
-        mat3 im3 = inverse(m3);
-        mat4 im4 = inverse(m4);
+		for (size_t i = 0; i < mat2_inverse_test_data.size(); i += 8) {
+			mat2 m1 = make_mat2(&mat2_inverse_test_data[i]);
+			mat2 im1 = inverse(m1);
 
-        assert(im2[0] == Approx2(-2, 1));
-        assert(im2[1] == Approx2(1.5, -0.5));
+			assert(im1[0] == Approx2(mat2_inverse_test_data[i + 4], mat2_inverse_test_data[i + 5]));
+			assert(im1[1] == Approx2(mat2_inverse_test_data[i + 6], mat2_inverse_test_data[i + 7]));
+		}
 
-        assert(im3[0] == Approx3(-0.194444448, 0.2777777791, 0.027777778));
-        assert(im3[1] == Approx3(0.055555556, -0.222222224, 0.277777791));
-        assert(im3[2] == Approx3(0.361111104, 0.0555555556, -0.194444448));
+		extern std::vector<float> mat3_inverse_test_data;
 
-        assert(im4[0] == Approx4(0.5, -0.5, -2, 1));
-        assert(im4[1] == Approx4(-1, 1, 3, -1));
-        assert(im4[2] == Approx4(-0.5, -0.5, 4, -1));
-        assert(im4[3] == Approx4(1, 0, -4, 1));
+		for (size_t i = 0; i < mat3_inverse_test_data.size(); i += 18) {
+			mat3 m1 = make_mat3(&mat3_inverse_test_data[i]);
+			mat3 im1 = inverse(m1);
+
+			assert(im1[0] == Approx3(mat3_inverse_test_data[i + 9], mat3_inverse_test_data[i + 10], mat3_inverse_test_data[i + 11]));
+			assert(im1[1] == Approx3(mat3_inverse_test_data[i + 12], mat3_inverse_test_data[i + 13], mat3_inverse_test_data[i + 14]));
+			assert(im1[2] == Approx3(mat3_inverse_test_data[i + 15], mat3_inverse_test_data[i + 16], mat3_inverse_test_data[i + 17]));
+		}
+
+		extern std::vector<float> mat4_inverse_test_data;
+
+		for (size_t i = 0; i < mat4_inverse_test_data.size(); i += 32) {
+			mat4 m1 = make_mat4(&mat4_inverse_test_data[i]);
+			mat4 im1 = inverse(m1);
+
+			assert(im1[0] == Approx4(mat4_inverse_test_data[i + 16], mat4_inverse_test_data[i + 17], mat4_inverse_test_data[i + 18], mat4_inverse_test_data[i + 19]));
+			assert(im1[1] == Approx4(mat4_inverse_test_data[i + 20], mat4_inverse_test_data[i + 21], mat4_inverse_test_data[i + 22], mat4_inverse_test_data[i + 23]));
+			assert(im1[2] == Approx4(mat4_inverse_test_data[i + 24], mat4_inverse_test_data[i + 25], mat4_inverse_test_data[i + 26], mat4_inverse_test_data[i + 27]));
+			assert(im1[3] == Approx4(mat4_inverse_test_data[i + 28], mat4_inverse_test_data[i + 29], mat4_inverse_test_data[i + 30], mat4_inverse_test_data[i + 31]));
+		}
     }
+
+	std::cout << "OK" << std::endl;
 }
 
 void test_transform() {
-    test_transform(math::Scalar);
+	std::random_device rd;
+	auto seed = rd();
+
+	std::cout << "test_transform (seed " << seed << ")" << std::endl;
+
+    test_transform(math::Scalar, seed);
 #ifndef __arm__
-    test_transform(math::SSE2);
-    test_transform(math::SSE3);
-    test_transform(math::SSE4_1);
-    test_transform(math::AVX);
+    test_transform(math::SSE2, seed);
+    test_transform(math::SSE3, seed);
+    test_transform(math::SSE4_1, seed);
+    test_transform(math::AVX, seed);
 #else
-    test_transform(math::NEON);
+    test_transform(math::NEON, seed);
 #endif
 }
